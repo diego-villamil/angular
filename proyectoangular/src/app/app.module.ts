d2b4e015cc5946c0a856b58f33e8e06a4208f5ea
goldwing4 <99834634+goldwing4@users.noreply.github.com>
2022-05-11 19:02:42 -0500

    first commit

---
+++ proyectoangular/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { PieComponent } from './componentes/pie/pie.component';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { EquipoComponent } from './paginas/equipo/equipo.component';
+import { ServiciosComponent } from './paginas/servicios/servicios.component';
+import { NosotrosComponent } from './paginas/nosotros/nosotros.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
+import { SinicioComponent } from './componentes/sinicio/sinicio.component';
+import { SnosotrosComponent } from './componentes/snosotros/snosotros.component';
+import { SequipoComponent } from './componentes/sequipo/sequipo.component';
+import { SserviciosComponent } from './componentes/sservicios/sservicios.component';
+import { ScontactoComponent } from './componentes/scontacto/scontacto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    MenuComponent,
+    PieComponent,
+    InicioComponent,
+    EquipoComponent,
+    ServiciosComponent,
+    NosotrosComponent,
+    ContactoComponent,
+    SinicioComponent,
+    SnosotrosComponent,
+    SequipoComponent,
+    SserviciosComponent,
+    ScontactoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ proyectoangular/src/app/componentes/scontacto/scontacto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scontacto',
+  templateUrl: './scontacto.component.html',
+  styleUrls: ['./scontacto.component.css']
+})
+export class ScontactoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
